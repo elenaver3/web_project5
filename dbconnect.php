1a85e493361d5170900a3f57a630232b35f3a7a1
elenaver3 <128687994+elenaver3@users.noreply.github.com>
2023-11-02 14:42:33 +0300

    second

--- dbconnect.php
+++ dbconnect.php
@@ -1,9 +1,4 @@
 <?php 
-    // define('DB_HOST', 'mysql://std-mysql.ist.mospolytech.ru:3306/std_2277_web_lab5');
-    // define('DB_HOST', 'https://phpmyadmin.fit.mospolytech.ru/');
-    // define('DB_USER', 'std_2277_web_lab5'); 
-    // define('DB_PASSWORD', 'web_lab5'); 
-    // define('DB_NAME', 'std_2277_web_lab5');
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root'); 
     define('DB_PASSWORD', ''); 
